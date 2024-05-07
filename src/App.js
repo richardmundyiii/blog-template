bae0736319d4a06341ee7c386045e1c76f36bfff
Madison Taylor <mundy.richard@icloud.com>
2024-05-07 09:31:24 -0700

    add Admin Nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import NavBar from "../src/components/NavBar/NavBar";
-import AdminPage from "../src/components/NavBar/NavBar";
+import AdminPage from "../src/pages/AdminPage/AdminPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<AdminPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </div>
   );
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import "./AdminPage.css";
+
 export default function AdminPage() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
     <>
-      <h2>Admin Page</h2>
+      <aside className="nav-left-container">
+        <div className="admin-top-container">
+          <h3>Admin</h3>
+          <hr />
+        </div>
+        <div className="admin-mid-container">
+          <ul>
+            <li>View All</li>
+            <li>Create</li>
+            <li>Settings</li>
+          </ul>
+        </div>
+        <div className="admin-bottom-container">
+          <hr />
+        </div>
+      </aside>
     </>
   );
 }
